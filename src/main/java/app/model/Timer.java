d385a1db8a0e8530c213b503a0ce0a471c4e33cd
stepanov-ssss <165507979+stepanov-ssss@users.noreply.github.com>
2024-06-26 19:27:32 +0300

    End work

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,10 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("myTimer")
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
